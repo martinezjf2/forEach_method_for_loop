33fb2e883eda10382298dc29837a6b697c42a424
Jeffrey Martinez <57267088+martinezjf2@users.noreply.github.com>
2021-03-14 20:06:06 -0400

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+// .
+// .
+// .
+// .
+// .
+// .
+
+let animals = ["Chicken", "Horse", "Pig", "Dog", "Cat"];
+
+animals.forEach((animal) => {
+  console.log(animal);
+});
+
+// for (let i = 0; i < animals.length; i++) {
+//   console.log(animals[i]);
+// }
